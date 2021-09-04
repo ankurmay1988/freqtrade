c21be9e8135d05154f215635a17d8f62ee88b0cb
ankurmay1988 <ankurakaet@gmail.com>
2021-09-04 07:00:04 +0000

    necessary modifications

---
+++ customindicators/pivotpoint.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+from pandas.core import series
+
+
+class PivotPoint:
+
+    def __pivotpoint(self, v: np.ndarray, leftbars: int, rightbars: int):
+        middle = leftbars
+        s = pd.Series(v - v.item(middle))
+        left = s[:middle]
+        right = s[middle+1:]
+        if right.size == rightbars:
+            if right.hasnans:
+                right = right.loc[right.notna()]
+
+            if np.all(left < 0) & np.all(right < 0):
+                return 1
+            elif np.all(left > 0) & np.all(right > 0):
+                return -1
+            else:
+                return np.nan
+        else:
+            return np.nan
+
+    def pivothigh(self, source: pd.Series, leftbars: int, rightbars: int):
+        pphl = self.pivotpoints(source, leftbars, rightbars)
+        return pd.Series(np.where(pphl == 1, pphl, np.nan))
+
+    def pivotlow(self, source: pd.Series, leftbars: int, rightbars: int):
+        pphl = self.pivotpoints(source, leftbars, rightbars)
+        return pd.Series(np.where(pphl == -1, pphl, np.nan))
+
+    def pivotpoints(self, source: pd.Series, leftbars: int, rightbars: int):
+        series = source.append(pd.Series(np.full(rightbars, np.nan)), ignore_index=True)
+        w = leftbars + rightbars + 1
+        pphl = series.rolling(w, min_periods=leftbars+2).apply(
+            raw=True, func=lambda x: self.__pivotpoint(x, leftbars, rightbars)).shift(-rightbars)
+        pphl = pphl.reindex(source.index)
+        return pd.Series(pphl)
---
+++ test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import customindicators as ci
+
+l = pd.Series([3, 1, 2, 3, 4, 5, 4, 2, 1, 2, 7, 5, 6, 2, 1, 0, 3])
+pphl = ci.PivotPoint().pivotpoints(l, 4, 4)
+print(pphl)
